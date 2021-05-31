f94dee857dc2f863930e1cc88bec13a985379bba
makiesse <ddociel@hotmail.com>
2021-05-31 18:26:21 -0300

    Criando a classe Dictionary

--- lib/src/main/java/br/com/softblue/jogoforca/core/Dictionary.java
+++ lib/src/main/java/br/com/softblue/jogoforca/core/Dictionary.java
@@ -1,7 +1,39 @@
 package br.com.softblue.jogoforca.core;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import br.com.softblue.jogoforca.game.GameException;
+import br.com.softblue.jogoforca.utils.RandomUtils;
+
+import java.util.List;
+
 public class Dictionary {
 	
+	private static final String FILE_NAME = "dictionario.txt";
 	
+	private List<String> words = new ArrayList<>();
+	
+	private void load() {
+		
+		try (Scanner scanner = 
+				new Scanner(getClass().getResourceAsStream("/" + FILE_NAME))){
+			
+			while (scanner.hasNextLine()) {
+				String word = scanner.nextLine().trim();
+				words.add(word);
+				
+			}
+			
+			if(words.size() == 0) {
+				throw new GameException("A lista de palavra não pode ser vazia");
+			}
+		}
+	}
+	
+	public Word nextWord() {
+		int pos = RandomUtils.newRandomNumber(0, words.size());
+		return new Word(words.get(pos));
+	}
 
 }
---
+++ lib/src/main/java/br/com/softblue/jogoforca/game/GameException.java
@@ -0,0 +1,15 @@
+package br.com.softblue.jogoforca.game;
+
+@SuppressWarnings("serial")
+public class GameException extends RuntimeException {
+
+	public GameException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public GameException(String message) {
+		
+	}
+
+
+}
